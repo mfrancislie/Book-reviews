e66c47e6821854c7984ee552b95e83d2206584b0
Francislie <mfrancislie@gmail.com>
2024-02-16 20:58:39 -0800

    implement popular and highestRated

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Contracts\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -15,8 +16,18 @@ class Book extends Model
     }
 
 
-    public function scopeTitle(Builder $query, string $title): Builder 
+    public function scopeTitle(Builder $query, string $title): Builder | QueryBuilder
     {
         return $query->where('title', 'LIKE', '%'. $title .'%');
+    }
+
+    public function scopePopular(Builder $query): Builder | QueryBuilder {
+
+    return $query->withCount('review')->orderBy('review_count', 'desc');            
+
+    }
+
+    public function scopeHighestRated(Builder $query): Builder | QueryBuilder {
+     return $query->withAvg('review', 'rating')->orderBy('review_avg_rating', 'desc');               
     }
 }
