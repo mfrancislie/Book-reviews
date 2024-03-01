03d5b71a9d8a3c9f020792f62dde0c13a72f25ef
Francislie <mfrancislie@gmail.com>
2024-02-29 20:03:39 -0800

    caching queries

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -28,7 +28,16 @@ class BookController extends Controller
             default => $books->latest()
             };
 
-      $books = $books->get();
+      $cacheKey = 'books:' . $filter . ':' .  $title;
+      $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
+     
+      /*
+        for testing cache 
+        $books = cache()->remember($cacheKey, 3600, function() use ($books) {
+            dd('No cache');
+            return $books->get();
+        }); 
+      */
     
         return view('books.index', ['books' => $books]);
 
