4617fee60e554e58e71a11339c2d6ce6739320d9
Francislie <mfrancislie@gmail.com>
2024-03-05 21:37:23 -0800

    invalidating cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -64,9 +64,15 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return  view('books.show', ['book' => $book->load([
+
+        $cacheKey = 'book:' . $book->id;
+
+        
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
             'review' => fn($query) => $query->latest()
-        ])]);
+        ]));
+
+        return  view('books.show', ['book' => $book]);
     }
 
     /**
